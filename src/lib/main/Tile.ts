af08ec225dbd002a3852f63c8b368a85c383b290
Javier <javier.huang168@gmail.com>
2023-12-19 13:18:17 -0500

    Add ring and tile

--- src/lib/main/Tile.ts
+++ src/lib/main/Tile.ts
@@ -1,5 +1,5 @@
-import type { TileType } from "$lib/enums/TileType";
-import type { BoxGeometry, Euler, Mesh, MeshLambertMaterial, Vector3 } from "three";
+import { TileType } from "$lib/enums/TileType";
+import { Vector3, BoxGeometry, type Euler, Mesh, MeshLambertMaterial } from "three";
 
 export class Tile {
     public static readonly LENGTH: number = 1;
@@ -9,10 +9,37 @@ export class Tile {
     public rotation: Euler;
     public dimensions: Vector3;
     
-    public type: TileType;
+    public type: number;
     public color: string;
 
     public mesh: Mesh;
     public geometry: BoxGeometry;
     public material: MeshLambertMaterial;
+
+    public constructor(position: Vector3, rotation: Euler, type: number) {
+        this.position = position;
+        this.rotation = rotation;
+        this.dimensions = new Vector3(Tile.LENGTH, Tile.HEIGHT, Tile.LENGTH);
+        this.type = type;
+        this.color = TileType.getColor(this.type);
+
+        this.spawn();
+    }
+
+    public spawn(): void {
+        this.geometry = new BoxGeometry(this.dimensions.x, this.dimensions.y, this.dimensions.z);
+        this.material = new MeshLambertMaterial({ color: this.color });
+        this.mesh = new Mesh(this.geometry, this.material);
+        this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+        this.mesh.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z);
+    }
+
+    public destroy(): void {
+        this.geometry.dispose();
+        this.material.dispose();
+
+        this.mesh.geometry.dispose();
+
+        this.mesh = null;
+    }
 }
