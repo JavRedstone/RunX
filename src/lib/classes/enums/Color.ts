1eaa6b7e6c8c310d4376d0edd002374fe0eb7e19
Javier <javier.huang168@gmail.com>
2023-12-21 16:47:32 -0500

    change color

--- src/lib/classes/enums/Color.ts
+++ src/lib/classes/enums/Color.ts
@@ -1,12 +1,13 @@
 export class Color {
-    public static readonly RED: string = '#ff0000';
-    public static readonly GREEN: string = '#00ff00';
-    public static readonly BLUE: string = '#0000ff';
-    public static readonly YELLOW: string = '#ffff00';
-    public static readonly CYAN: string = '#00ffff';
-    public static readonly MAGENTA: string = '#ff00ff';
-    public static readonly ORANGE: string = '#ff8000';
-    public static readonly PURPLE: string = '#8000ff';
+    public static readonly RED: string = '#f56e53';
+    public static readonly GREEN: string = '#a9f553';
+    public static readonly BLUE: string = '#5399f5';
+    public static readonly YELLOW: string = '#f5e553';
+    public static readonly CYAN: string = '#53f5e2';
+    public static readonly MAGENTA: string = '#ed53f5';
+    public static readonly ORANGE: string = '#f5ba53';
+    public static readonly PURPLE: string = '#c453f5';
+    public static readonly BEIGE: string = '#f5d753';
     public static readonly GRAY: string = '#808080';
     public static readonly WHITE: string = '#ffffff';
     public static readonly BLACK: string = '#000000';
